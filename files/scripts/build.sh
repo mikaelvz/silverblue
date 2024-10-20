52bdbfb668a78ba1fa80f09b5b9f58a016383905
mikaelvz <mikael@mvz.fr>
2024-10-20 12:15:48 +0200

    moved ectool install from script to recipe

--- files/scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ouex pipefail
-
-# Install ectool
-curl -fL https://files.tree123.org/utils/x86_64/gnu/ectool -o /usr/bin/ectool
-chmod a+x /usr/bin/ectool
-
-systemctl enable ectool-fanspeed
